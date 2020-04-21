3689c59c9834362e6adfe05dd0877f852ef4f128
0x00developer <info@tecnoayuda.co>
2020-04-21 15:57:32 -0500

    task0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,8 +5,13 @@ my_app = Flask(__name__)
 
 
 @my_app.route('/', strict_slashes=False)
+""" decorator """
+
+
 def my_hello():
+    """ function """
     return 'Hello, HBNB!'
 
 if __name__ == "__main__":
+    """ conditional """
     my_app.run(host="0.0.0.0", port=5000)
