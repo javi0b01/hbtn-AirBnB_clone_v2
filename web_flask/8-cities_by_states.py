f7517622b2ac5a49a36ad514cc075bff50852add
0x00developer <info@tecnoayuda.co>
2020-04-22 13:12:50 -0500

    task9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Task 9. Cities by states """
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    states = list(storage.all(State).values())
+    return render_template("8-cities_by_states.html", states=states)
+
+
+@app.teardown_appcontext
+def to_close(err):
+    """ close app context """
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
